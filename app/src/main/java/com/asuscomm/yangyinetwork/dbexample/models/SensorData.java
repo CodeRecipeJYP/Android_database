3687eb69cc6111c6f683ad895f6fb84c92504524
pjo901018 <pjo901018@korea.ac.kr>
2017-06-19 15:47:45 +0900

    Dto정의

--- app/src/main/java/com/asuscomm/yangyinetwork/dbexample/models/SensorData.java
+++ app/src/main/java/com/asuscomm/yangyinetwork/dbexample/models/SensorData.java
@@ -1,8 +1,22 @@
 package com.asuscomm.yangyinetwork.dbexample.models;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Created by jaeyoung on 2017. 6. 19..
  */
 
+@DatabaseTable(tableName = "sensor_datas")
 public class SensorData {
+    @DatabaseField(generatedId = true)
+    int id;
+    @DatabaseField
+    String device;
+    @DatabaseField
+    double value;
+
+    public SensorData() {
+        // ORMLite 필수
+    }
 }
