037877c61d84dd3e68fec6d554c3d85e113940a6
pjo901018 <pjo901018@korea.ac.kr>
2017-06-19 20:03:08 +0900

    GlobalApplication 추가

---
+++ app/src/main/java/com/asuscomm/yangyinetwork/dbexample/utils/GlobalApplication.java
@@ -0,0 +1,14 @@
+package com.asuscomm.yangyinetwork.dbexample.utils;
+
+import android.app.Application;
+
+/**
+ * Created by jaeyoung on 2017. 6. 19..
+ */
+
+public class GlobalApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
